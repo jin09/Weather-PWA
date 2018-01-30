7413433cfa8628d8d785a4896fdaba9b6d6b3468
Gautam Jain <supergautam995@gmail.com>
2018-01-30 14:58:17 +0530

    step 2 - caching the static files

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,35 @@
 /**
  * Created by gautam on 30/1/18.
  */
+var cacheName = 'weatherPWA-v1';
+
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/scripts/localforage-1.4.0.js',
+  '/styles/ud811.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
